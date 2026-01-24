27a00b43fbc16d5b6f3057a585c6719062c7607d
Sebastian Yeo <khyeo81@gmail.com>
2026-01-23 20:55:43 -0800

    fix

--- backend/app.py
+++ backend/app.py
@@ -35,5 +35,5 @@ def get_poll_logs():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
     
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
