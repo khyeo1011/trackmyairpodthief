7ecff9a8326da8416e89989cfa6bfc972f51de7f
Sebastian Yeo <khyeo81@gmail.com>
2026-01-24 09:54:52 -0800

    log

--- backend/app.py
+++ backend/app.py
@@ -12,22 +12,24 @@ app = Flask(__name__)
 CORS(app)
 DB_NAME = "../locations.db"
 
+
 def get_db_connection():
     conn = sqlite3.connect(DB_NAME)
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/api/poll-logs', methods=['GET'])
+
+@app.route("/api/poll-logs", methods=["GET"])
 def get_poll_logs():
     now = datetime.now()
+    default_start = (now - timedelta(hours=24)).strftime("%Y-%m-%d %H:%M:%S")
     logging.info(f"Request received at {now}")
-    default_start = (now - timedelta(hours=24)).strftime('%Y-%m-%d %H:%M:%S')
-    
-    start_time = request.args.get('start', default=default_start)
-    end_time = request.args.get('end', default=now.strftime('%Y-%m-%d %H:%M:%S'))
-    part_filter = request.args.get('part') # New filter
-    limit = request.args.get('limit', default=100, type=int)
-    offset = request.args.get('offset', default=0, type=int)
+
+    start_time = request.args.get("start", default=default_start)
+    end_time = request.args.get("end", default=now.strftime("%Y-%m-%d %H:%M:%S"))
+    part_filter = request.args.get("part")  # New filter
+    limit = request.args.get("limit", default=100, type=int)
+    offset = request.args.get("offset", default=0, type=int)
 
     # Base query
     query = "SELECT * FROM poll_logs WHERE timestamp BETWEEN ? AND ?"
@@ -44,9 +46,19 @@ def get_poll_logs():
         with get_db_connection() as conn:
             logs = conn.execute(query, params).fetchall()
             logging.info(f"Fetched {len(logs)} logs, head : {dict(logs[0])}")
-        return jsonify({"status": "success", "count": len(logs), "data": [dict(row) for row in logs]}), 200
+        return (
+            jsonify(
+                {
+                    "status": "success",
+                    "count": len(logs),
+                    "data": [dict(row) for row in logs],
+                }
+            ),
+            200,
+        )
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
-    
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000, host="0.0.0.0")
